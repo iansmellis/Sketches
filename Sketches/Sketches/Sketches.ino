fb4c2ab9480166ea1fb80317bf72413717919824
iansmellis <iansmellis@gmail.com>
2016-04-29 16:51:12 -0500

    Added EMPTY.ino as blank test sketch

---
+++ Sketches/Sketches/Sketches.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Sketches.ino
+ Created:	4/29/2016 4:14:02 PM
+ Author:	Smelly
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
