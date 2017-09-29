45b692d89dd3bf7aa711ca5191e9c84961afa097
Ian <iansmellios@gmail.com>
2017-09-29 12:26:24 -0500

    Update EMPTY.ino

--- EMPTY/EMPTY.ino
+++ EMPTY/EMPTY.ino
@@ -1,13 +1,15 @@
-void setup()
-{
+/*
+ Name:		Sketches.ino
+ Created:	4/29/2016 4:14:02 PM
+ Author:	Smelly
+*/
 
-  /* add setup code here */
+// the setup function runs once when you press reset or power the board
+void setup() {
 
 }
 
-void loop()
-{
-
-  /* add main program code here */
-
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
 }
