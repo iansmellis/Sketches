9d1efd0cfc21e482e4dfca1a95368d6c8fe360e7
Ian <iansmellios@gmail.com>
2017-09-29 12:21:01 -0500

    Update BLINK.ino

--- BLINK/BLINK.ino
+++ BLINK/BLINK.ino
@@ -1,18 +1,36 @@
-// the setup function runs once when you press reset or power the board
+/*  SparkFun Inventor's Kit for Photon
+    Experiment 1 - Part 1: Hello World Blink an LED
+    This sketch was written by SparkFun Electronics
+    August 31, 2015
+    https://github.com/sparkfun
+    Modified by Ian Ellis
+    September 29, 2017
+
+    This is a simple example sketch that turns on an LED 
+    for one second, off for one second, and repeats forever.
+
+    Development environment specifics:
+    Particle Build environment (https://www.particle.io/build)
+    Particle Photon RedBoard
+    Released under the MIT License(http://opensource.org/licenses/MIT)
+*/
+
+int led = D7; // LED is connected to D7
+
+// This function runs once when you press reset or power the board
 void setup()
 {
-	Serial.begin(115200);
-	// initialize digital pin 13 as an output.
-	pinMode(13, OUTPUT);
+	Serial.begin(115200);   // Initialize Serial monitor connection and baud rate
+	pinMode(led, OUTPUT);   // Initialize D7 pin as output
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-	digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-	Serial.println("On");
-	delay(1000);              // wait for a second
-	digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-	Serial.println("Off");
-	delay(1000);              // wait for a second
+	digitalWrite(led, HIGH);    // Turn ON the LED (HIGH is the voltage level)
+	Serial.println("On");       // Print to Serial Monitor "On"
+	delay(1000);                // Wait for 1000mS = 1 second
+	digitalWrite(led, LOW);     // Turn OFF the LED
+	Serial.println("Off");      // Print to Serial Monitor "Off"
+	delay(1000);                // Wait for 1 second
 }
